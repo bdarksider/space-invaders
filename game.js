ba39d259b92b31330d3e28eb75a6edad79b86c09
Bejoy George Mathew <bejoygm@gmail.com>
2018-02-23 20:47:04 +0530

    draw player

--- game.js
+++ game.js
@@ -4,6 +4,8 @@
 		var screen = canvas.getContext('2d');
 		var gameSize = {x: canvas.width, y: canvas.height };
 
+		this.bodies = [new Player(this, gameSize)];
+
 		var self = this;
 		var tick = function() {
 			self.update();
@@ -21,9 +23,30 @@
 		},
 
 		draw: function(screen, gameSize) {
-			screen.fillRect(30, 30, 40, 40);
+			for (var i = 0; i < this.bodies.length; i++) {
+				drawRect(screen, this.bodies[i]);
+			}
 		}
 	};
+
+	var Player = function(game, gameSize) {
+		this.game = game;
+		this.size = {x: 15, y: 15};
+		this.center = {x: gameSize.x / 2, y: gameSize.y - this.size.x };
+	};
+
+	Player.prototype = {
+		update: function() {
+
+		}
+	};
+
+	// draws al the bodies in screen
+	var drawRect = function(screen, body) {
+		screen.fillRect(body.center.x - body.size.x / 2,
+						body.center.y - body.size.y / 2,
+						body.size.x, body.size.y)
+	}
 
 	window.onload = function() {
 		new Game("screen");
