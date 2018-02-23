d6a61dc3d2cadbf35968f9d927fa33aadb006627
Bejoy George Mathew <bejoygm@gmail.com>
2018-02-23 21:32:38 +0530

    added bullets

--- game.js
+++ game.js
@@ -25,9 +25,14 @@
 		},
 
 		draw: function(screen, gameSize) {
+			screen.clearRect(0, 0, gameSize.x, gameSize.y);
 			for (var i = 0; i < this.bodies.length; i++) {
 				drawRect(screen, this.bodies[i]);
 			}
+		},
+
+		addBody: function(body) {
+			this.bodies.push(body);
 		}
 	};
 
@@ -40,11 +45,35 @@
 
 	Player.prototype = {
 		update: function() {
+			// move left and right
 			if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
 				this.center.x -= 2;
 			} else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
 				this.center.x += 2;
 			}
+
+			// fire bullets
+			if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+				var bullet = new Bullet(
+					{x: this.center.x, y: this.center.y - this.size.x /2 },
+					{x: 0, y: -6}
+				);
+
+				this.game.addBody(bullet);
+			}
+		}
+	};
+
+	var Bullet = function(center, velocity) {
+		this.size = {x: 3, y: 3};
+		this.center = center;
+		this.velocity = velocity;
+	};
+
+	Bullet.prototype = {
+		update: function() {
+			this.center.x += this.velocity.x;
+			this.center.y += this.velocity.y;
 		}
 	};
 
